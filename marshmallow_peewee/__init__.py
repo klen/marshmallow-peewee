c909e729fbcad0147a10e57facc363798715ce30
Kirill Klenov <horneds@gmail.com>
2016-06-29 16:19:42 +0300

    Bump version: 0.2.8 → 1.0.0

--- marshmallow_peewee/__init__.py
+++ marshmallow_peewee/__init__.py
@@ -1,6 +1,6 @@
 __license__ = "MIT"
 __project__ = "Marshmallow-Peewee"
-__version__ = "0.2.8"
+__version__ = "1.0.0"
 
 from .schema import ModelSchema # noqa
 from .convert import Related    # noqa
