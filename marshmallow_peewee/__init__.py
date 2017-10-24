c08bcebb70579a35489a665143d5e0d72ecf97f2
Kirill Klenov <horneds@gmail.com>
2017-10-24 18:53:36 +0300

    Bump version: 1.2.3 → 1.2.4

--- marshmallow_peewee/__init__.py
+++ marshmallow_peewee/__init__.py
@@ -1,6 +1,6 @@
 __license__ = "MIT"
 __project__ = "Marshmallow-Peewee"
-__version__ = "1.2.3"
+__version__ = "1.2.4"
 
 from .schema import ModelSchema # noqa
 from .fields import Timestamp, MSTimestamp, Related, ForeignKey # noqa
